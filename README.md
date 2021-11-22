6b27cb89ba2b51df77138376036075b80aa93769
Hoan To <hoan.to@eng-its.de>
2021-11-22 15:22:09 +0100

    put token into file

---
+++ README.md
@@ -0,0 +1,16 @@
+# How to use
+
+This is a script to connect to CKAN
+
+## Create User for CKAN
+Login to https://auth.dlr.wobcom.tech, login with admin credentials (should be provided) and create a User, which can then be used for ckan
+
+## Get User Token
+
+* Login to CKAN with previously created account
+* click on your profile, at the top right
+* click API Tokens, and create an API Token
+
+## Use token in script
+
+Take the previous token and copy it into a file named 'ckan-token.txt'. The Script should now work.
--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import requests, json, getpass
 
 def getData():
 
+    file = open('ckan-token.txt', 'r')
+    token = file.readlines().pop(0)
+
     test_api_url = "https://ckan.dlr.wobcom.tech/api/3/action/package_list"
-    api_call_headers = {'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2Mzc1ODYzMTIsImp0aSI6IlJMYUY1UUVvYVFKLXEtQWttZTF3bEJHNE9Sa0RMeXlmY2FMWTJ0NVR5dDB2ZmN1TjV6ejRjRHRCSkNrUWdDeEhkVVJ0cWNVUWhicHYtMFRPIn0.pimCCGBm9bkRCQ6qG96dAqGZyU8jKORu8vBwOJY3VmM'}
+    api_call_headers = {'Authorization': token}
     api_call_response = requests.get(test_api_url, headers=api_call_headers, verify=False)
     return api_call_response.text
-
 if __name__ == '__main__':
     print(getData())
